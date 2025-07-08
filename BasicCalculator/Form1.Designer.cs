1cda638e9548654915908d1ad3b04e96c918a101
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-07-08 16:58:17 -0700

    Refactor TextBox controls for integer input

--- BasicCalculator/Form1.Designer.cs
+++ BasicCalculator/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             label1 = new Label();
             label2 = new Label();
-            LengthTbx = new TextBox();
-            WidthTbx = new TextBox();
+            Integer1Tbx = new TextBox();
+            Integer2Tbx = new TextBox();
             CalculateBtn = new Button();
-            AreaTbx = new TextBox();
+            SumTbx = new TextBox();
             label3 = new Label();
-            textBox1 = new TextBox();
+            OperandTbx = new TextBox();
             SuspendLayout();
             // 
             // label1
@@ -43,32 +43,32 @@
             label1.AutoSize = true;
             label1.Location = new Point(56, 51);
             label1.Name = "label1";
-            label1.Size = new Size(44, 15);
+            label1.Size = new Size(56, 15);
             label1.TabIndex = 0;
-            label1.Text = "Length";
+            label1.Text = "Integer 1:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(56, 118);
+            label2.Location = new Point(56, 135);
             label2.Name = "label2";
-            label2.Size = new Size(39, 15);
+            label2.Size = new Size(56, 15);
             label2.TabIndex = 1;
-            label2.Text = "Width";
+            label2.Text = "Integer 2:";
             // 
-            // LengthTbx
+            // Integer1Tbx
             // 
-            LengthTbx.Location = new Point(178, 48);
-            LengthTbx.Name = "LengthTbx";
-            LengthTbx.Size = new Size(100, 23);
-            LengthTbx.TabIndex = 2;
+            Integer1Tbx.Location = new Point(178, 48);
+            Integer1Tbx.Name = "Integer1Tbx";
+            Integer1Tbx.Size = new Size(100, 23);
+            Integer1Tbx.TabIndex = 2;
             // 
-            // WidthTbx
+            // Integer2Tbx
             // 
-            WidthTbx.Location = new Point(178, 115);
-            WidthTbx.Name = "WidthTbx";
-            WidthTbx.Size = new Size(100, 23);
-            WidthTbx.TabIndex = 3;
+            Integer2Tbx.Location = new Point(178, 132);
+            Integer2Tbx.Name = "Integer2Tbx";
+            Integer2Tbx.Size = new Size(100, 23);
+            Integer2Tbx.TabIndex = 3;
             // 
             // CalculateBtn
             // 
@@ -76,45 +76,45 @@
             CalculateBtn.Name = "CalculateBtn";
             CalculateBtn.Size = new Size(144, 71);
             CalculateBtn.TabIndex = 4;
-            CalculateBtn.Text = "Calculate Area and Perimeter";
+            CalculateBtn.Text = "Calculate Sum";
             CalculateBtn.UseVisualStyleBackColor = true;
             CalculateBtn.Click += CalculateBtn_Click;
             // 
-            // AreaTbx
+            // SumTbx
             // 
-            AreaTbx.Location = new Point(56, 301);
-            AreaTbx.Name = "AreaTbx";
-            AreaTbx.ReadOnly = true;
-            AreaTbx.Size = new Size(222, 23);
-            AreaTbx.TabIndex = 5;
+            SumTbx.Location = new Point(56, 301);
+            SumTbx.Name = "SumTbx";
+            SumTbx.ReadOnly = true;
+            SumTbx.Size = new Size(222, 23);
+            SumTbx.TabIndex = 5;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(56, 84);
+            label3.Location = new Point(56, 93);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(56, 15);
             label3.TabIndex = 6;
-            label3.Text = "label3";
+            label3.Text = "Operand:";
             // 
-            // textBox1
+            // OperandTbx
             // 
-            textBox1.Location = new Point(178, 81);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 7;
+            OperandTbx.Location = new Point(178, 90);
+            OperandTbx.Name = "OperandTbx";
+            OperandTbx.Size = new Size(100, 23);
+            OperandTbx.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
+            Controls.Add(OperandTbx);
             Controls.Add(label3);
-            Controls.Add(AreaTbx);
+            Controls.Add(SumTbx);
             Controls.Add(CalculateBtn);
-            Controls.Add(WidthTbx);
-            Controls.Add(LengthTbx);
+            Controls.Add(Integer2Tbx);
+            Controls.Add(Integer1Tbx);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
@@ -127,11 +127,11 @@
 
         private Label label1;
         private Label label2;
-        private TextBox LengthTbx;
-        private TextBox WidthTbx;
+        private TextBox Integer1Tbx;
+        private TextBox Integer2Tbx;
         private Button CalculateBtn;
-        private TextBox AreaTbx;
+        private TextBox SumTbx;
         private Label label3;
-        private TextBox textBox1;
+        private TextBox OperandTbx;
     }
 }
