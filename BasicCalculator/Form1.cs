6f790d9a1fb7c0df8cd715191776a22115a3f721
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-07-08 17:06:37 -0700

    Add arithmetic operations to CalculateBtn_Click method
    Enhanced the `CalculateBtn_Click` method to perform
    addition, subtraction, multiplication, and division based
    on user input. Implemented error handling for division
    by zero and invalid operands, with results displayed in
    the designated text box.

--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -9,7 +9,37 @@ public partial class Form1 : Form
 
     private void CalculateBtn_Click(object sender, EventArgs e)
     {
-        
+        int value1 = Convert.ToInt32(Integer1Tbx.Text);
+        int value2 = Convert.ToInt32(Integer2Tbx.Text);
+        string operand = OperandTbx.Text.Trim();
+        int result = 0;
+
+
+        switch (operand)
+        {
+            case "+":
+                result = value1 + value2;
+                break;
+            case "-":
+                result = value1 - value2;
+                break;
+            case "*":
+                result = value1 * value2;
+                break;
+            case "/":
+                if (value2 != 0)
+                    result = value1 / value2;
+                else
+                {
+                    SumTbx.Text = "Error: Divide by zero";
+                    return;
+                }
+                break;
+            default:
+                SumTbx.Text = "Error: Invalid operand";
+                return;
+        }
+        SumTbx.Text = result.ToString();
     }
 }
 
